4c0db58eac4b94b231d90b38f4eca82946938fe3
YongLeeCode <yonghwan311@gmail.com>
2024-11-26 12:26:38 +0900

    feat: 카카오 문제 해결 클래스 생성

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import kakaoCodingTest.EnglishWordWithStrDigit;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // 0~9까지 혹은 zero~nine까지 영문 혹은 숫자로 구성된 String
+        // String에 영단어로 숫자를 기입한 것을 숫자로 바꾸기
+        String input = "onetwo34one9nine";
+        EnglishWordWithStrDigit.solution(input);
     }
 }
---
+++ src/kakaoCodingTest/EnglishWordWithStrDigit.java
@@ -0,0 +1,44 @@
+package kakaoCodingTest;
+
+import java.util.Map;
+
+public class EnglishWordWithStrDigit {
+
+    public static void solution(String s) {
+        String[] characters = s.split("");
+
+        //map
+        Map<String, Integer> hashNum = Map.of(
+                "one", 1,
+                "two", 2,
+                "three", 3,
+                "four", 4,
+                "five", 5,
+                "six", 6,
+                "seven", 7,
+                "eight", 8,
+                "nine", 9,
+                "zero", 0
+        );
+
+        String jo = "";
+        String newStrNum = "";
+        for (String character : characters) {
+            jo += character;
+
+            //match method
+            if (jo.matches("\\d")){
+                newStrNum += jo;
+                jo = "";
+            }
+            if (hashNum.containsKey(jo)) {
+                newStrNum += hashNum.get(jo);
+                jo = "";
+            }
+        }
+        System.out.println(newStrNum);
+
+
+    }
+
+}
