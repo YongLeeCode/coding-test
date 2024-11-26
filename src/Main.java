26a74e99a39ca092a7d1994b5ab0ba849347bbb7
YongLeeCode <yonghwan311@gmail.com>
2024-11-26 21:40:07 +0900

    feat: 새로 만든 클래스를 위한 메인 코드 변경

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-import kakaoCodingTest.EnglishWordWithStrDigit;
+import baekjoonCodingTest.LuckyStraight;
+import javaApi.ApiEquals;
 
 public class Main {
 
@@ -6,7 +7,12 @@ public class Main {
 
         // 0~9까지 혹은 zero~nine까지 영문 혹은 숫자로 구성된 String
         // String에 영단어로 숫자를 기입한 것을 숫자로 바꾸기
-        String input = "onetwo34one9nine";
-        EnglishWordWithStrDigit.solution(input);
+//        String input = "onetwo34one9nine";
+//        EnglishWordWithStrDigit.solveProblem(input);
+
+//        ApiEquals.testWithString();
+//        ApiEquals.testWithList();
+//        int input = 55;
+//        LuckyStraight.solveProblem(input);
     }
 }
