c3c71763ea1c9c0b002240700227813226b4657f
YongLeeCode <yonghwan311@gmail.com>
2024-12-02 21:26:20 +0900

    feat: 숫자 문자열과 영단어 구현

--- src/kakaoCodingTest/engilsh_word_with_string_digit/EnglishWordWithStrDigit.java
+++ src/kakaoCodingTest/engilsh_word_with_string_digit/EnglishWordWithStrDigit.java
@@ -1,13 +1,12 @@
-package kakaoCodingTest;
+package kakaoCodingTest.engilsh_word_with_string_digit;
 
 import java.util.Map;
 
 public class EnglishWordWithStrDigit {
 
-    public static void solveProblem(String s) {
+    public static String solveProblem(String s) {
         String[] characters = s.split("");
 
-        //map
         Map<String, Integer> hashNum = Map.of(
                 "one", 1,
                 "two", 2,
@@ -22,22 +21,28 @@ public class EnglishWordWithStrDigit {
         );
 
         String jo = "";
-        String newStrNum = "";
+        StringBuilder newStrNum = new StringBuilder();
         for (String character : characters) {
             jo += character;
 
-            //match method
-            if (jo.matches("\\d")){
-                newStrNum += jo;
+            if (jo.matches("\\d")) {
+                newStrNum.append(jo);
                 jo = "";
             } else if (hashNum.containsKey(jo)) {
-                newStrNum += hashNum.get(jo);
+                newStrNum.append(hashNum.get(jo));
                 jo = "";
             }
         }
-        System.out.println(newStrNum);
 
+        return newStrNum.toString();
+    }
 
+    public static void main(String[] args) {
+        // 0~9까지 혹은 zero~nine까지 영문 혹은 숫자로 구성된 String
+        // String에 영단어로 숫자를 기입한 것을 숫자로 바꾸기
+        String input = "onetwo34one9nine";
+        String strNumber = EnglishWordWithStrDigit.solveProblem(input);
+        System.out.println(strNumber);
     }
 
 }
