21d9e7e4e83bf227027f211530f55fd1f03bf10a
YongLeeCode <yonghwan311@gmail.com>
2024-12-02 21:48:08 +0900

    feat: 콜라 문제 구현

--- src/recursiveFunction/how_many_cola_return/Cola.java
+++ src/recursiveFunction/how_many_cola_return/Cola.java
@@ -1,17 +1,25 @@
-package recursiveFunction.cola.how_many_cola_return;
+package recursiveFunction.how_many_cola_return;
 
 public class Cola {
     static int newColaCount = 0;
 
-    public static int solveProblem(int a, int b, int n) {
+    public static int solution(int a, int b, int n) {
         int newCola = n / a * b;
         int emptyBottle = n % a + n / a * b;
 
         newColaCount += newCola;
 
         if(n / a > 0){
-            solveProblem(a, b, emptyBottle);
+            solution(a, b, emptyBottle);
         }
         return newColaCount;
+    }
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 1;
+        int n = 20;
+        int output = solution(a, b, n);
+        System.out.println(output);
     }
 }
