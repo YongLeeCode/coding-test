3981b8f41ee03dfab46c009ab5a680e110ac1d45
YongLeeCode <yonghwan311@gmail.com>
2025-01-18 22:37:31 +0900

    feat: 문제 생성

---
+++ src/exam3/JumpCase.java
@@ -0,0 +1,68 @@
+package exam3;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LongJump {
+    //시간 초과
+//    public static long solution(int n) {
+//        long result = dfs(n, 0) % 1234567;
+//        return result;
+//    }
+//
+//    private static int dfs(int n, int curr) {
+//        if(curr == n) {
+//            return 1;
+//        }
+//        if(curr > n) {
+//            return 0;
+//        }
+//
+//        return dfs(n, curr + 1) + dfs(n, curr + 2);
+//    }
+
+
+    //bfs로 만들 수 있나? 고민하다가 막힘
+//    public static long solution(int n) {
+//        long result = bfs(n) %1234567;
+//        return result;
+//    }
+//
+//    private static int bfs(int n) {
+//        int count = 0;
+//        Queue<Integer> queue = new LinkedList<>();
+//
+//        while(!queue.isEmpty()) {
+//            int curr = 0;
+//            for(int i = 0; i < queue.size(); i++) {
+//                curr += queue.poll();
+//            }
+//            if(n > curr + 2) {
+//                queue.offer(2);
+//            }
+//
+//            queue.offer(1);
+//
+//        }
+//    }
+
+    //DP 방법
+    public static long solution(int n) {
+        long[] answer = new long[2000];
+        answer[1] = 1;
+        answer[2] = 2;
+
+        for(int i = 3; i < answer.length; i++) {
+            answer[i] = answer[i - 1] + answer[i - 2] % 1234567;
+        }
+
+        return answer[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        long result = solution(n);
+        System.out.println(result);
+    }
+}
