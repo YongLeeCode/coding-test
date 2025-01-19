4d997da3387bc5102821cb2afd10b7614ac30106
YongLeeCode <yonghwan311@gmail.com>
2025-01-19 14:31:47 +0900

    feat: 집털이, 정수 삼각형, 단속카메라 문제 생성

---
+++ src/dynamicProgramming/HouseRobber.java
@@ -0,0 +1,23 @@
+package greedy;
+
+// 출처: leetcode
+// 문제 제목: House Robber
+public class HouseRobber {
+    public static int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+
+        dp[0] = nums[0];
+        dp[1] = nums[1];
+
+        for(int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1], nums[i] + nums[i - 2]);
+        }
+        return dp[nums.length - 1];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 9, 3, 1};
+        int result = rob(nums);
+        System.out.println(result);
+    }
+}
