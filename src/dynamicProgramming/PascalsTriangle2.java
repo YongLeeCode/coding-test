19fdbb941c1d17577799fcc34d26918d4699367e
YongLeeCode <yonghwan311@gmail.com>
2025-01-22 12:31:28 +0900

    feat: Pascal's Triangle 2 문제 구현

--- src/dynamicProgramming/PascalsTriangle2.java
+++ src/dynamicProgramming/PascalsTriangle2.java
@@ -1,4 +1,33 @@
 package dynamicProgramming;
 
+// 출처: leetcode
+// 문제 제목: Pascal's Triangle
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PascalsTriangle2 {
+    public static List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> rows = new ArrayList<>();
+        rows.add(0, new ArrayList<>(List.of(0)));
+        for(int i = 0; i <= rowIndex; i++) {
+            List<Integer> list = new ArrayList<>();
+            for(int j = 0; j <= i; j++) {
+                if(j == 0) {
+                    list.add(1);
+                } else if (j == i) {
+                    list.add(1);
+                } else {
+                    list.add(rows.get(i - 1).get(j - 1) + rows.get(i - 1).get(j));
+                }
+            }
+            rows.add(i, list);
+        }
+        return rows.get(rowIndex);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> result = getRow(3);
+        System.out.println(result);
+    }
 }
