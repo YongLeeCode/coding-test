c0521ed192d28ced22d2dba254d71a5a3d834823
YongLeeCode <yonghwan311@gmail.com>
2025-01-29 11:16:45 +0900

    feat: 포켓몬, 영어단어 끝말잇기 문제 구현

--- src/exam4/Pocketmon.java
+++ src/exam4/Pocketmon.java
@@ -1,4 +1,20 @@
 package exam4;
 
+
+import java.util.HashMap;
+
 public class Pocketmon {
+    public static int solution(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        return Math.min(nums.length / 2, map.size());
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,3,3,2,2, 2};
+        int result = solution(nums);
+        System.out.println(result);
+    }
 }
--- src/exam4/WordsTrain.java
+++ src/exam4/WordsTrain.java
@@ -1,4 +1,33 @@
 package exam4;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class WordsTrain {
+    // HashSet을 사용해서 이미 사용한 단어인지 확인
+    // 만약 사용했다면
+    //  그 위치 인덱스 + 1 % n = 어떤 사람이 실수했는지 알 수 있다.
+    //  인덱스 + 1 / n을 하면 몇번째 회전에서 실수했는지 알 수 있다.
+    public static int[] solution(int n, String[] words) {
+        HashSet<String> memorize = new HashSet<>();
+        char letter = words[0].charAt(0);
+        for (int i = 0; i < words.length; i++) {
+            if(memorize.contains(words[i]) || words[i].charAt(0) != letter) {
+                int person = (i % n) + 1;
+                int rotation = (i / n) + 1;
+                System.out.println(person + " " + rotation);
+                return new int[]{person, rotation};
+            }
+            letter = words[i].charAt(words[i].length() - 1);
+            memorize.add(words[i]);
+        }
+
+        return new int[]{0, 0};
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"hello", "one", "even", "never", "now", "world", "draw"};
+        int[] result = solution(2, words);
+        System.out.println(Arrays.toString(result));
+    }
 }
