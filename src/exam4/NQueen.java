f0abe940dd9b9bcaaf0a22a0900c1e2adef582b1
YongLeeCode <yonghwan311@gmail.com>
2025-02-08 23:43:32 +0900

    feat: 프로그래머스 N퀸 문제 구현

--- src/exam4/NQueen.java
+++ src/exam4/NQueen.java
@@ -1,4 +1,45 @@
 package exam4;
 
-public class AndWolf {
+public class NQueen {
+
+    private static int answer;
+    private static int[] board;
+
+    public static int solution(int n) {
+        board = new int[n];
+        findSafeZone(0, n);
+        return answer;
+    }
+
+    private static void findSafeZone(int index, int n) {
+        if (index >= n) {
+            answer++;
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            board[index] = i;
+            if (valid(index)) {
+                findSafeZone(index + 1, n);
+            }
+        }
+    }
+
+    private static boolean valid(int index) {
+        for (int j = 0; j < index; j++) {
+            if (board[index] == board[j]) {
+                return false;
+            }
+            if (Math.abs(index - j) == Math.abs(board[index] - board[j])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int result = solution(4);
+        System.out.println(result);
+    }
 }
